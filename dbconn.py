6f8f2eed9046a492e46b6990ebb31e99b154f094
DBAN <dban7171@gmail.com>
2021-11-15 23:24:07 +0900

    Feat : 모든 API 호출 구현
    현재 API 호출 까지는 구현 했지만 디테일 적인 부분들은 구현 x
    
    유저가 테이블 속 특정 데이터를 검색할 수 있는 기능빼곤 모두 구현 완료
    이 부분은 README에 어떻게 해결할 것인지 서술해 놓았음

--- dbconn.py
+++ dbconn.py
@@ -6,8 +6,8 @@ sql = db_auth.app
 
 class engineconn:
 
-    def __init__(self,dbname : str):
-        self.engine = create_engine(sql['name'] + '://' + sql['user']+':'+sql['password']+'@'+sql['host']+':'+sql['port']+'/'+dbname, pool_recycle =500)
+    def __init__(self):
+        self.engine = create_engine(sql['name'] + '://' + sql['user']+':'+sql['password']+'@'+sql['host']+':'+sql['port']+'/'+sql['db'], pool_recycle =500)
 
     def sessionmaker(self):
         Session = sessionmaker(bind=self.engine)
--- rest_api.py
+++ rest_api.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sqlalchemy import MetaData, Table, Column, Integer,VARCHAR,BigInteger
+from sqlalchemy import MetaData, Table, Column, Integer, VARCHAR, BigInteger
 from sqlalchemy.orm import declarative_base
 from dbconn import engineconn
-from typing import Optional
-
 import uvicorn
 
 
 Base = declarative_base()
 metadata = MetaData()
-engine = engineconn('synthea_1000')
+engine = engineconn()
 session = engine.sessionmaker()
 connect = engine.connection()
 app = FastAPI()
-
 
 
 #---------- CLASS 선언부 ----------
@@ -27,7 +24,6 @@ class Person(Base):
     race_source_value = Column(VARCHAR(50),nullable=True)
     ethnicity_source_value = Column(VARCHAR(50),nullable=True)
     year_of_birth = Column(Integer,nullable=True)
-
 
     def __repr__(self):
         return "<User(person_id='%s', gender_concept_id='%s', race_source_value='%s', " \
@@ -45,6 +41,30 @@ class Visit(Base):
         return "<User(visit_occurrence_id='%s', person_id='%s', visit_concept_id='%s')>" % (
             self.visit_occurrence_id,self.person_id, self.visit_concept_id)
 
+class Drug(Base):
+
+    __tablename__ = 'drug_exposure'
+    drug_exposure_id = Column(BigInteger,nullable=False,primary_key=True)
+    person_id = Column(BigInteger,nullable=True)
+    drug_concept_id = Column(Integer,nullable=True)
+    visit_occurrence_id = Column(BigInteger, nullable=True)
+
+    def __repr__(self):
+        return "<User(drug_exposure_id='%s', person_id='%s', drug_concept_id='%s, visit_occurrence_id='%s')>" % (
+            self.drug_exposure_id,self.person_id, self.drug_concept_id, self.visit_occurrence_id)
+
+class Condition(Base):
+
+    __tablename__ = 'condition_occurrence'
+    condition_occurrence_id = Column(BigInteger,nullable=False,primary_key=True)
+    person_id = Column(BigInteger,nullable=True)
+    condition_concept_id = Column(Integer,nullable=True)
+    visit_occurrence_id = Column(BigInteger, nullable=True)
+
+    def __repr__(self):
+        return "<User(person_id='%s', condition_concept_id='%s', visit_occurrence_id='%s')>" % (
+            self.person_id,self.condition_concept_id, self.visit_occurrence_id)
+
 class Concept(Base):
 
     __tablename__ = 'concept'
@@ -55,84 +75,82 @@ class Concept(Base):
     def __repr__(self):
         return "<User(concept_id='%s', concept_name='%s', domain_id='%s')>" % (
             self.concept_id,self.concept_name, self.domain_id)
+
 # POST Class
 class Gender(BaseModel):
+    # 5207 5232
     gender : str
 
 class Race(BaseModel):
+    # white, asian, black
     color : str
 
 class Ethinicity(BaseModel):
+    # hispanic, nonhispanic
     nation : str
 
 class Occurrence(BaseModel):
+    # 9201, 9202, 9203
     visit : str
 
 class Off_limit(BaseModel):
     offset: int
     limit: int
 
-# ------------------------ CLASS ---------------------------------
+# --------------------------------- CLASS ---------------------------------
 
+# --------------------------------- API ---------------------------------
 @app.get('/person/all')
 async def person_all():
     table = Table('person', metadata, autoload=True, autoload_with=engine.engine)
-    person = session.query(table).all()
+    person = session.query(table).count()
     return person
 
-#성별
-@app.post('/person/sex')
-async def gender(sex : Gender):
-    #8507 male 8532 female
+@app.post('/person/gender')
+async def gender(sex: Gender):
     result = session.query(Person.gender_concept_id).filter(Person.gender_concept_id == sex.gender).count()
 
     return result
 
-#인종
 @app.post('/person/race')
-async def race_color(race : Race):
+async def race_color(race: Race):
     # white, black, asian
     result_color = session.query(Person.race_source_value).filter(Person.race_source_value == race.color).count()
 
     return result_color
 
-# 민족
 @app.post('/person/ethinicity')
-async def ethinicity_nation(ethin : Ethinicity):
+async def ethinicity_nation(ethin: Ethinicity):
     result_nation = session.query(Person.ethnicity_source_value).filter(Person.ethnicity_source_value == ethin.nation).count()
 
     return result_nation
 
-# 사망
 @app.get('/person/death')
 async def death():
     table = Table('death', metadata, autoload=True, autoload_with=engine.engine)
     death_person = session.query(table).count()
     return death_person
 
-# 환자 유형 (입원, 외래, 응급)
 @app.post('/person/visit')
-async def visit_occurrence(occurrence : Occurrence):
-#9201 02 03
+async def visit_occurrence(occurrence: Occurrence):
     result_occurrence = session.query(Visit).filter(Visit.visit_concept_id == occurrence.visit).count()
 
     return result_occurrence
 
-@app.post('/person/visitor/gender')
+@app.post('/person/visit/gender')
 async def visitor(sex: Gender):
     result_visitor = session.query(Visit, Person).filter(Visit.person_id == Person.person_id,Person.gender_concept_id == sex.gender).count()
 
     return result_visitor
 
-@app.post('/person/visitor/race')
+@app.post('/person/visit/race')
 async def visitor(race: Race):
-    # 인종별
     result_visitor_race = session.query(Visit, Person).filter(Visit.person_id == Person.person_id,Person.race_source_value == race.color).count()
 
     return result_visitor_race
 
-@app.post('/person/visitor/nation')
-async def visitor(ethin : Ethinicity):
+@app.post('/person/visit/nation')
+async def visitor(ethin: Ethinicity):
     result_visitor = session.query(Visit, Person).filter(Visit.person_id == Person.person_id,Person.ethnicity_source_value == ethin.nation).count()
 
     return result_visitor
@@ -145,12 +163,29 @@ async def age_visitor(age: int):
     return result
 
 @app.post('/concept/{search}')
-async def concept(search : str, cnt :Off_limit):
+async def concept(search: str, cnt: Off_limit):
     result_concept = session.query(Concept).filter(Concept.domain_id == search).offset(cnt.offset).limit(cnt.offset).all()
 
     return result_concept
 
+@app.post('/row/{table}')
+async def concept_row(row: str, cnt: Off_limit):
 
+    if row == 'Drug':
+        result_concept = session.query(Drug).offset(cnt.offset).limit(cnt.offset).all()
+
+    elif row == 'Condition':
+        result_concept = session.query(Condition).offset(cnt.offset).limit(cnt.offset).all()
+
+    elif row == 'Visit':
+        result_concept = session.query(Visit).offset(cnt.offset).limit(cnt.offset).all()
+
+    elif row == 'Person':
+        result_concept = session.query(Person).offset(cnt.offset).limit(cnt.offset).all()
+
+    return result_concept
+
+# --------------------------------- API ---------------------------------
 
 if __name__ == '__main__':
     uvicorn.run(app)
